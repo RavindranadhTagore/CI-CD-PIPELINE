b856a7e0f4389b2d4a339f06a25976d02f0faacf
RavindranadhTagore <137029385+RavindranadhTagore@users.noreply.github.com>
2025-04-24 18:19:43 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,7 +4,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1>CI/CD Deployed Successfully with Python!</h1>"
-            
+    return """
+    <html>
+        <head>
+            <title>CI/CD Success</title>
+        </head>
+        <body style="display: flex; flex-direction: column; justify-content: center; align-items: center; height: 100vh; margin: 0;">
+            <h1>CI/CD Pipeline</h1>
+            <p>Deployed Successfully with Python!</p>
+        </body>
+    </html>
+    """
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
