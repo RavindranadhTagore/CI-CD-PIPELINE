d50aa6a0466405aad4a1a3892da10c8710556871
RavindranadhTagore <137029385+RavindranadhTagore@users.noreply.github.com>
2025-04-24 17:45:35 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,7 +5,6 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "<h1>CI/CD Deployed Successfully with Python!</h1>"
-             "<p>CI/CD stands for Continuous Integration and Continuous Deployment (or Continuous Delivery). It is a software development practice that automates the integration, testing, and deployment of code changes, enabling faster and more reliable delivery of applications</p>"
-
+            
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
